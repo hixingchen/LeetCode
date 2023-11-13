45cbd6987ca391e3eb26e5ffa9b26ce94c84031c
hixingchen <2781003217@qq.com>
2023-11-13 20:53:45 +0800

    hixingchen

---
+++ src/com/hixingchen/leetCode/简单/L2609_最长平衡子字符串/Solution.java
@@ -0,0 +1,21 @@
+package com.hixingchen.leetCode.简单.L2609_最长平衡子字符串;
+
+public class Solution {
+    public int findTheLongestBalancedSubstring(String s) {
+        int res = 0;
+        int[] count = new int[2];
+        count[s.charAt(0) == '1'?1:0] = 1;
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == '0' && s.charAt(i-1) == '1'){
+                count[0] = 1;
+                count[1] = 0;
+            }else if (s.charAt(i) == '0'){
+                count[0]++;
+            }else {
+                count[1]++;
+            }
+            res = Math.max(res,Math.min(count[0],count[1]));
+        }
+        return res*2;
+    }
+}
