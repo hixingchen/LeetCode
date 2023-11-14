c06859805a93359589d6b32a08ebf86e17a57ada
hixingchen <2781003217@qq.com>
2023-11-14 13:47:10 +0800

    hixingchen

--- src/com/hixingchen/leetCode/Main.java
+++ src/com/hixingchen/leetCode/Main.java
@@ -1,11 +1,11 @@
 package com.hixingchen.leetCode;
-import com.hixingchen.leetCode.中等.L16_最接近的三数之和.Solution;
+//import com.hixingchen.leetCode.中等.L16_最接近的三数之和.Solution;
 //import com.hixingchen.leetCode.困难.L1163_按字典序排在最后的子串.Solution;
 //import com.hixingchen.leetCode.简单.L2427_公因子的数目.Solution;
+import com.hixingchen.leetCode.简单题测试.Solution;
 public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        int[] nums = new int[]{1,1,1,0};
-        solution.threeSumClosest(nums,-100);
+        String [] strings = {"i","love","leetcode","apples"};
     }
 }
--- src/com/hixingchen/leetCode/简单题测试/Solution.java
+++ src/com/hixingchen/leetCode/简单题测试/Solution.java
@@ -1,13 +1,23 @@
 package com.hixingchen.leetCode.简单题测试;
 
-import com.hixingchen.leetCode.中等.L1145_二叉树着色游戏.TreeNode;
+import java.util.*;
 
-import java.lang.reflect.Field;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-class Solution {
-
+public class Solution {
+    public int findTheLongestBalancedSubstring(String s) {
+        int res = 0;
+        int[] count = new int[2];
+        count[s.charAt(0) == '1'?1:0] = 1;
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == '0' && s.charAt(i-1) == '1'){
+                count[0] = 1;
+                count[1] = 0;
+            }else if (s.charAt(i) == '0'){
+                count[0]++;
+            }else {
+                count[1]++;
+            }
+            res = Math.max(res,Math.min(count[0],count[1]));
+        }
+        return res*2;
+    }
 }
