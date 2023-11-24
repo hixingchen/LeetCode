04de52a964f967682b1ab0738d171e727f48219b
hixingchen <2781003217@qq.com>
2023-11-24 09:13:44 +0800

    hixingchen

--- src/com/hixingchen/leetCode/简单/L2418_按身高排序/Solution.java
+++ src/com/hixingchen/leetCode/简单/L2418_按身高排序/Solution.java
@@ -1,6 +1,6 @@
-package com.hixingchen.leetCode.简单题测试;
+package com.hixingchen.leetCode.简单.L2418_按身高排序;
 
-import java.util.*;
+import java.util.Arrays;
 
 public class Solution {
     public String[] sortPeople(String[] names, int[] heights) {
@@ -8,7 +8,7 @@ public class Solution {
         for (int i = 0; i < heightTemp.length; i++) {
             heightTemp[i] = i;
         }
-        Arrays.sort(heightTemp,(a,b)->heights[b]-heights[a]);
+        Arrays.sort(heightTemp,(a, b)->heights[b]-heights[a]);
         String[] result = new String[names.length];
         for (int i = 0; i < result.length; i++) {
             result[i] = names[heightTemp[i]];
