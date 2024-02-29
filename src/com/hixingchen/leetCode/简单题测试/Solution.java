8317e4833a35797800a9007f8a879c13aaa9da89
hixingchen <2781003217@qq.com>
2024-02-29 12:36:47 +0800

    hixingchen

--- src/com/hixingchen/leetCode/简单题测试/Solution.java
+++ src/com/hixingchen/leetCode/简单题测试/Solution.java
@@ -3,8 +3,5 @@ package com.hixingchen.leetCode.简单题测试;
 import java.util.*;
 
 public class Solution {
-    public boolean isBoomerang(int[][] points) {
-        Arrays.sort(points,(a,b)->a[0]-b[0]);
-        return !(points[2][0]-points[1][0] == points[1][0]-points[0][0] && points[2][1]-points[1][1] == points[1][1]-points[0][1]) && (points[0][0] != points[1][0]&& points[0][0]!=points[2][0]);
-    }
+
 }
