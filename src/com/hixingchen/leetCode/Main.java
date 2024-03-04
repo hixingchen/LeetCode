3334193319e44aeb1824a6cda4927e85c7720352
hixingchen <2781003217@qq.com>
2024-03-04 18:19:10 +0800

    hixingchen

--- src/com/hixingchen/leetCode/Main.java
+++ src/com/hixingchen/leetCode/Main.java
@@ -13,6 +13,5 @@ import java.util.Map;
 public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        solution.minLength("ABFCACDB");
     }
 }
