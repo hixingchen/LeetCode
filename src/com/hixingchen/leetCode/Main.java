aedbd6faaec1a87eb00c566f876df5e735619801
hixingchen <2781003217@qq.com>
2024-03-06 10:56:09 +0800

    hixingchen

--- src/com/hixingchen/leetCode/Main.java
+++ src/com/hixingchen/leetCode/Main.java
@@ -13,5 +13,6 @@ import java.util.Map;
 public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
+        solution.sumOddLengthSubarrays(new int[]{1,4,2,5,3});
     }
 }
--- src/com/hixingchen/leetCode/简单题测试/Solution.java
+++ src/com/hixingchen/leetCode/简单题测试/Solution.java
@@ -3,15 +3,18 @@ package com.hixingchen.leetCode.简单题测试;
 import java.util.*;
 
 public class Solution {
-    public int[] countBits(int n) {
-        int[] bits = new int[n + 1];
-        int highBit = 0;
-        for (int i = 1; i < n; i++) {
-            if ((i&i-1) == 0){
-                highBit = i;
+    public int sumOddLengthSubarrays(int[] arr) {
+        int len = arr.length;
+        int res = 0;
+        for (int i = 1; i <= len; i++) {
+            int temp = 0;
+            int sum = 0;
+            while (temp<len){
+                sum += Math.min(len-(Math.max(1,i-temp)+temp)+1,temp+1);
+                temp+=2;
             }
-            bits[i] = bits[i-highBit]+1;
+            res += sum*arr[i-1];
         }
-        return bits;
+        return res;
     }
 }
