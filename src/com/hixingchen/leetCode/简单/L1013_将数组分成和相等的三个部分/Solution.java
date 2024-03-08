2063b3f1e557effbbbd65fa198dba68692fa6f97
hixingchen <2781003217@qq.com>
2024-03-08 15:34:08 +0800

    hixingchen

---
+++ src/com/hixingchen/leetCode/简单/L1013_将数组分成和相等的三个部分/Solution.java
@@ -0,0 +1,34 @@
+package com.hixingchen.leetCode.简单.L1013_将数组分成和相等的三个部分;
+
+public class Solution {
+    public boolean canThreePartsEqualSum(int[] A) {
+        int s = 0;
+        for (int num : A) {
+            s += num;
+        }
+        if (s % 3 != 0) {
+            return false;
+        }
+        int target = s / 3;
+        int n = A.length, i = 0, cur = 0;
+        while (i < n) {
+            cur += A[i];
+            if (cur == target) {
+                break;
+            }
+            ++i;
+        }
+        if (cur != target) {
+            return false;
+        }
+        int j = i + 1;
+        while (j + 1 < n) {  // 需要满足最后一个数组非空
+            cur += A[j];
+            if (cur == target * 2) {
+                return true;
+            }
+            ++j;
+        }
+        return false;
+    }
+}
