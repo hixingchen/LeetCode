38aaafa6a8363474fb1165f1cf42b7d802fc8b46
hixingchen <2781003217@qq.com>
2024-03-13 11:09:29 +0800

    hixingchen

---
+++ src/com/hixingchen/leetCode/简单/L572_另一棵树的子树/Solution.java
@@ -0,0 +1,24 @@
+package com.hixingchen.leetCode.简单.L572_另一棵树的子树;
+
+public class Solution {
+    public boolean isSubtree(TreeNode s, TreeNode t) {
+        return dfs(s, t);
+    }
+
+    public boolean dfs(TreeNode s, TreeNode t) {
+        if (s == null) {
+            return false;
+        }
+        return check(s, t) || dfs(s.left, t) || dfs(s.right, t);
+    }
+
+    public boolean check(TreeNode s, TreeNode t) {
+        if (s == null && t == null) {
+            return true;
+        }
+        if (s == null || t == null || s.val != t.val) {
+            return false;
+        }
+        return check(s.left, t.left) && check(s.right, t.right);
+    }
+}
---
+++ src/com/hixingchen/leetCode/简单/L572_另一棵树的子树/TreeNode.java
@@ -0,0 +1,14 @@
+package com.hixingchen.leetCode.简单.L572_另一棵树的子树;
+
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode() {}
+    public TreeNode(int val) { this.val = val; }
+    public TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
