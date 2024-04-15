79159a5df3ae54d24ebd08a986e5c055744844a2
hixingchen <2781003217@qq.com>
2024-04-15 19:11:42 +0800

    hixingchen

---
+++ src/com/hixingchen/leetCode/简单/L2965_找出缺失和重复的数字/Solution.java
@@ -0,0 +1,23 @@
+package com.hixingchen.leetCode.简单.L2965_找出缺失和重复的数字;
+
+public class Solution {
+    public int[] findMissingAndRepeatedValues(int[][] grid) {
+        int n = grid.length;
+        int[] cnt = new int[n * n + 1];
+        for (int[] row : grid) {
+            for (int x : row) {
+                cnt[x]++;
+            }
+        }
+
+        int[] ans = new int[2];
+        for (int i = 1; i <= n * n; i++) {
+            if (cnt[i] == 2) {
+                ans[0] = i;
+            } else if (cnt[i] == 0) {
+                ans[1] = i;
+            }
+        }
+        return ans;
+    }
+}
--- src/com/hixingchen/leetCode/简单题测试/Solution.java
+++ src/com/hixingchen/leetCode/简单题测试/Solution.java
@@ -5,4 +5,5 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Solution {
+
 }
