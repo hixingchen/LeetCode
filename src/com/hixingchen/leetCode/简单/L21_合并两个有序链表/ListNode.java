df00ff2377cfd4014f1520176cacc30322a8205e
hixingchen <2781003217@qq.com>
2024-04-18 14:22:05 +0800

    hixingchen

---
+++ src/com/hixingchen/leetCode/简单/L21_合并两个有序链表/ListNode.java
@@ -0,0 +1,9 @@
+package com.hixingchen.leetCode.简单.L21_合并两个有序链表;
+
+public class ListNode {
+    public int val;
+    public ListNode next;
+    public ListNode(int x){
+        val = x;
+    }
+}
--- src/com/hixingchen/leetCode/简单题测试/Solution.java
+++ src/com/hixingchen/leetCode/简单题测试/Solution.java
@@ -5,35 +5,29 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Solution {
-    public int storeWater(int[] bucket, int[] vat) {
-        int len = bucket.length;
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->b[0]-a[0]);
-        int cnt = 0;
-        for (int i = 0; i < len; i++) {
-            if (bucket[i] == 0 && vat[i] != 0){
-                cnt++;
-                bucket[i]++;
-            }
-            if (vat[i]>0){
-                pq.offer(new int[]{(vat[i]+bucket[i]-1)/bucket[i],i});
-            }
-        }
-        if (pq.isEmpty()){
-            return 0;
-        }
-        int res = Integer.MAX_VALUE;
-        while (cnt<res){
-            int[] arr = pq.poll();
-            int v = arr[0];
-            int i = arr[1];
-            res = Math.min(res,cnt+v);
-            if (v<=2){
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode res = new ListNode(0);
+        ListNode temp = res;
+        while (true){
+            ListNode temp2 = new ListNode(0);
+            if (list1 == null){
+                temp.next = list2;
                 break;
             }
-            cnt++;
-            bucket[i]++;
-            pq.offer(new int[]{(vat[i]+bucket[i]-1)/bucket[i],i});
+            if (list2 == null){
+                temp.next = list1;
+                break;
+            }
+            if (list1.val>list2.val){
+                temp2.val = list2.val;
+                list2 = list2.next;
+            }else {
+                temp2.val = list1.val;
+                list1 = list1.next;
+            }
+            temp.next = temp2;
+            temp = temp2;
         }
-        return res;
+        return res.next;
     }
 }
