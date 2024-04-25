2ccd21b7c9946462db57616f3d5d92dfa954bc5d
hixingchen <2781003217@qq.com>
2024-04-25 17:48:47 +0800

    hixingchen

--- src/com/hixingchen/leetCode/Main.java
+++ src/com/hixingchen/leetCode/Main.java
@@ -14,6 +14,5 @@ import java.util.Map;
 public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        solution.checkStraightLine(new int[][]{{2,1},{4,2},{6,3}});
     }
 }
