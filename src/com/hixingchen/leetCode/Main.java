f2ef77c336696742ac57ffc965d7e3dee0e15fb4
hixingchen <2781003217@qq.com>
2024-05-17 15:22:51 +0800

    hixingchen

--- src/com/hixingchen/leetCode/Main.java
+++ src/com/hixingchen/leetCode/Main.java
@@ -2,11 +2,7 @@ package com.hixingchen.leetCode;
 //import com.hixingchen.leetCode.中等.L16_最接近的三数之和.Solution.java;
 //import com.hixingchen.leetCode.困难.L1163_按字典序排在最后的子串.Solution.java;
 //import com.hixingchen.leetCode.简单.L2427_公因子的数目.Solution.java;
-import com.hixingchen.leetCode.简单题测试.ListNode;
 import com.hixingchen.leetCode.简单题测试.Solution;
-import com.hixingchen.leetCode.简单题测试.TreeNode;
-
-import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
--- src/com/hixingchen/leetCode/简单/L1863_找出所有子集的异或总和再求和/Solution.java
+++ src/com/hixingchen/leetCode/简单/L1863_找出所有子集的异或总和再求和/Solution.java
@@ -1,8 +1,8 @@
-package com.hixingchen.leetCode.简单题测试;
+package com.hixingchen.leetCode.简单.L1863_找出所有子集的异或总和再求和;
 
-import java.util.Arrays;
-import java.util.List;
-
+/**
+ * 按位考虑 + 二项式展开(比较难理解)
+ */
 public class Solution {
     public int subsetXORSum(int[] nums) {
         int res = 0;
