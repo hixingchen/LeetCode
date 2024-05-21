da465323804f413a26ba937bbb1a376c64220653
hixingchen <2781003217@qq.com>
2024-05-21 16:41:30 +0800

    hixingchen

--- src/com/hixingchen/leetCode/简单题测试/Solution.java
+++ src/com/hixingchen/leetCode/简单题测试/Solution.java
@@ -1,22 +1,19 @@
 package com.hixingchen.leetCode.简单题测试;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.PriorityQueue;
+import java.util.*;
 
 public class Solution {
-    public int[] supplyWagon(int[] supplies) {
-        int len = supplies.length/2;
-        for (int i = supplies.length; i>len; i--) {
-            int temp = 1;
-            for (int j = 1; j < i; j++) {
-                if (supplies[j]+supplies[j-1]<supplies[temp]+supplies[temp-1]){
-                    temp = j;
-                }
-            }
-            supplies[temp-1] += supplies[temp];
-            System.arraycopy(supplies,temp+1,supplies,temp,i-temp-1);
-        }
-        return Arrays.copyOf(supplies,len);
+    TreeNode target;
+    public boolean findTarget(TreeNode root, int k) {
+        target = root;
+        return find2(root,k);
+    }
+    private boolean find(TreeNode root,int k){
+        if (root == null) return false;
+        return k == root.val||find(root.left,k)||find(root.right,k);
+    }
+    public boolean find2(TreeNode root, int k) {
+        if (root == null) return false;
+        return find2(root.right,k)||find(target,k-root.val)||find2(root.left,k);
     }
 }
